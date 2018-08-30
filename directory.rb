21abaed808ab6dca6685a04f369d413c9e18cf5f
James <jamie@shelis-air.home>
2018-08-30 13:22:37 +0100

    register with added hobbies, countires and height

--- directory.rb
+++ directory.rb
@@ -1,14 +1,23 @@
 student_count = 11
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names, then hobbies,then country of birst and then
+  height of the students"
+  puts "To finish, just hit return four times"
   students = []
   name = gets.chomp
+  hobbies = gets.chomp
+  country_of_birth = gets.chomp
+  height = gets.chomp
+
 
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobbies: hobbies,
+      country_of_birth: country_of_birth, height: height }
     puts "Now we have #{students.count} students"
     name = gets.chomp
+    hobbies = gets.chomp
+    country_of_birth = gets.chomp
+    height = gets.chomp
   end
   students
 end
@@ -19,7 +28,9 @@ def print_header
 end
 def print(students)
     students.each.with_index(1) do |student, index|
-    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort), who
+    likes #{student[:hobbies]}, is from #{student[:country_of_birth]} and is
+    #{student[:height]} tall"
   end
 end
 def print_footer(names)
