a690c751ae605d34b463b51e1d608c9a96668bec
James <jamie@Shelis-MacBook-Air.local>
2018-09-02 22:01:39 +0100

    grouped by cohort month

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+$months = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
+$width = 50
 def default_string(string)
   string = gets.chomp
   if string.empty?
@@ -5,7 +7,6 @@ def default_string(string)
   end
   return string
 end
-
 def input_students
   puts "Please enter student name"
   puts "to finish hit enter twice"
@@ -13,9 +14,8 @@ def input_students
   name = gets.chomp
   while !name.empty? do
     puts "Enter cohort"
-    months = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
     cohort = default_string(cohort).downcase.to_sym
-    unless months.include? cohort
+    while !$months.include? cohort
       puts "Please try again"
       puts "Enter cohort"
       cohort = default_string(cohort).downcase.to_sym
@@ -28,7 +28,7 @@ def input_students
     height = default_string(height)
 
     students << {name: name, cohort: cohort, hobbies: hobbies,
-      country_of_birth: country_of_birth, height: height }
+      country_of_birth: country_of_birth, height: height}
     puts "Now we have #{students.count} students"
     puts "Please enter another student name"
     puts "to finish hit return twice"
@@ -41,24 +41,26 @@ def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
-def print(students)
-  i = 0
-  while i < students.length
-    width = 50
-      spacer = ' '
-      puts "#{students.index(students[i]) + 1}: #{students[i][:name]}"
-      puts " Who likes #{students[i][:hobbies]} ".center(width,spacer)
-      puts " Is from: #{students[i][:country_of_birth]} ".center(width,spacer)
-      puts " (Height: #{students[i][:height]} tall) ".center(width,spacer)
-      puts " (#{students[i][:cohort]} cohort) ".center(width,spacer)
+def printer(students)
+  $months.map do |month|
+    students.map do |student|
+      unless !student[:cohort].to_s.include? month.to_s
+        i = 0
+        puts "#{student[:name]}"
+        puts " Who likes #{student[:hobbies]} ".center($width)
+        puts " Is from: #{student[:country_of_birth]} ".center($width)
+        puts " Height: #{student[:height]} tall ".center($width)
+        puts " #{student[:cohort]} cohort ".center($width)
         i += 1
+      end
+    end
   end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students
 print_header
-print(students)
+printer(students)
 print_footer(students)
